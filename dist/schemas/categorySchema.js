7f4d72069a0a6c25acd564c40c1157cb32643423
HieuK1502 <hoanghieu7a3@gmail.com>
2025-08-15 16:32:37 +0700

    chore(config): refactor project config to use env variables for deployment again

--- dist/schemas/categorySchema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createCategorySchema = void 0;
-const zod_1 = require("zod");
-exports.createCategorySchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, { message: "Name is required" }),
-});
--- dist/schemas/commentSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateCommentSchema = exports.createCommentSchema = void 0;
-const zod_1 = require("zod");
-exports.createCommentSchema = zod_1.z.object({
-    content: zod_1.z.string().min(1, { message: "Content là bắt buộc" }),
-});
-exports.updateCommentSchema = exports.createCommentSchema;
