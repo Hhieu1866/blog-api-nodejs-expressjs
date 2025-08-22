5958b158b0e5a908f9344a47864eb01fb106b429
HieuK1502 <hoanghieu7a3@gmail.com>
2025-08-22 10:41:28 +0700

    refactor(server): reposition error handler middleware to ensure proper error handling order and enhance error logging in middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,13 +2,14 @@ import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (
   err: any,
-  _req: Request,
+  req: Request,
   res: Response,
-  _next: NextFunction,
+  next: NextFunction
 ) => {
-  console.error("Error:", err);
+  console.error(err.stack);
 
   res.status(err.status || 500).json({
     message: err.message || "Internal Server Error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 };
